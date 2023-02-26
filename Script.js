a46f7a40ee1edd6aaad5989f61f097ca7ecd1b4d
aleksandr-melnik <melnik.aleksandr.02@gmail.com>
2023-02-27 00:59:43 +0300

    reinit

--- Script.js
+++ Script.js
@@ -1,10 +1,10 @@
 const getRandomNumInRange = (min, max) => {
-    const randomNum = (Math.random() * (max - min) + min).toFixed(0) 
+    const randomNum = (Math.random() * (max - min) + min).toFixed(0)
     return randomNum
 }
 const getTask = () => {
-    const symbol =  (Math.random() > 0.5) ? '+' : '-'
-    const task = `${getRandomNumInRange (0, 100)} ${symbol} ${getRandomNumInRange (0, 100)}`
+    const symbol = (Math.random() > 0.5) ? '+' : '-'
+    const task = `${getRandomNumInRange(0, 100)} ${symbol} ${getRandomNumInRange(0, 100)}`
     gameState.rightAnswer = eval(task)
     return task
 }
@@ -22,7 +22,7 @@ const gameState = {
     rightAnswer: null,
 }
 const StartGameFunc = () => {
-   if (!gameState.taskInProcess) {
+    if (!gameState.taskInProcess) {
         title.innerText = 'Игра началась!'
         userAnswer.value = null
         userTask.innerText = getTask()
@@ -42,12 +42,12 @@ userAnswer.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
         StartGameFunc()
     } else if (e.key === 'Escape') {
-        userAnswer.blur() 
+        userAnswer.blur()
     }
 })
 
 const choosedEl = document.querySelectorAll('.choosed_block-container > div')
-const counterEl = document.querySelector('.choosed_block span') 
+const counterEl = document.querySelector('.choosed_block span')
 const choosedState = {
     countElements: 0,
 }
@@ -66,5 +66,47 @@ const eventFunc = (e) => {
 }
 for (let i = 0; i < choosedEl.length; i++) {
     choosedEl[i].addEventListener('click', eventFunc)
-}      
- 
+}
+const postBlock = document.querySelector('.posts_block-container')
+const showPostsBTN = document.querySelector('.posts_block button')
+const func = () => 5
+
+function addPost(title, body) {
+    const postTitle = document.createElement('h3')
+    const postBody = document.createElement('span')
+    const postItem = document.createElement('p')
+
+    postTitle.innerText = title
+    postBody.innerText = body
+
+    postItem.append(postTitle, postBody)
+    postBlock.append(postItem)
+}
+function getPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => {
+            for (item of data) {
+                addPost(item.title, item.body)
+            }
+            // addPost(data[7].title, data[7].body)
+        })
+        .catch(err => console.log(err.message))
+}
+// function createPost(title, body, userId) {
+//     fetch('https://jsonplaceholder.typicode.com/posts', {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             title: title,
+//             body: body,
+//             userId: userId,
+//         }),
+//         headers: {
+//             'Content-type': 'application/json; charset=UTF-8'
+//         },
+//     })
+//         .then(res => res.json())
+//         .catch(err => console.log(err.message))
+// }
+
+showPostsBTN.onclick = getPosts
